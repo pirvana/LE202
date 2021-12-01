e371dfd3d842aaba775a3de0f40ab7bede4e718c
pirvana <pirvana@example.com>
2021-12-01 18:29:16 -0500

    Contenedor django configurado para usar contenedor de mariadb

--- project/settings.py
+++ project/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'mariadb',
+        'PORT': '',
     }
 }
 
